b37db765e8a95d33bb918b626cc62d454169601c
YonYoon <zhzhzh1806@gmail.com>
2024-01-03 14:26:22 +0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Zhansen Zhalel on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
